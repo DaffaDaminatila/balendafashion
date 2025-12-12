d8340b7a9b723267f368dfb1adff13622335dd62
Muhammad Daffa Daminatila <91118761+DaffaDaminatila@users.noreply.github.com>
2025-12-12 19:57:45 +0700

    Add base URL and plugins to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+export default defineConfig({
+   base: '/balendafashion/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+})
